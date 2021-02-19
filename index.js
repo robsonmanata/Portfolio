e9ed259b5dadd6aa571dc289f14cdd2a764d2694
robsonmanata <70637849+robsonmanata@users.noreply.github.com>
2021-02-19 17:25:57 +0100

    add send message capability and .env file

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
+require("dotenv").config();
 const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
+const fs = require("fs");
+
+var transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "robsonmanata@gmail.com",
+    pass: process.env.GMAILPASS_AUTHENTICATE
+  },
+  tls: {
+         rejectUnauthorized: false
+     }
+});
 
 
 const app = express();
@@ -24,6 +38,43 @@ app.get("/portfolio",(req, res)=> {
 app.get("/blog",(req, res)=> {
   res.render("blog");
 });
+
+app.post("/",function(req,res){
+  if(req.body.message){
+
+    var mailOptions = {
+    from: req.body.emailaddress,
+    to: "robsonmanata@gmail.com",
+    subject:"message from rob-portfolio sent by " + req.body.emailaddress,
+    text:"my name is " + req.body.firstname + " service requested [" + req.body.service + "]  " +req.body.message,
+    attachments: [
+       {
+
+         filename:req.body.attachment ,
+            content: req.body.attachment
+         // filename: req.body.attachment,
+         //  content: fs.createReadStream("Downloads/" + req.body.attachment)
+       }
+     ]
+  };
+
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+console.log(req.body.attachment);
+  res.redirect("/");
+  }
+  else{
+    res.redirect("/");
+  }
+
+});
+
+
 let port = process.env.PORT;
 if(port == null || port == ""){
   port = 3000;
